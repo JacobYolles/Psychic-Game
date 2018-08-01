4819403a3f4333e5dae31f0d440b9171953a53ae
Jacob Yolles <jacobyolles@Jacobs-MacBook-Pro.local>
2018-08-01 11:44:16 -0600

    Second Edit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,16 @@
 var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+var userGuess
 var loggedGuesses = []
 var wins
 var losses
 var guessesRemaining = 9;
 
+
+// function reset() {
+// 	computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+//     return computerGuess;
+// }
 
 
 //Here is the on run function
@@ -14,7 +21,20 @@ document.onkeyup = function(event) {
 
     if (userGuess === computerGuess) {
         wins++;
+        console.log(wins)
     }   else if (userGuess != computerGuess) {
         guessesRemaining--
+        console.log(guessesRemaining)
     }
 }
+    if (guessesRemaining === 0) {
+
+    }
+
+
+
+    document.getElementById("yourGuess").textContent = userGuess
+    document.getElementById("loggedGuesses").textContent = loggedGuesses
+    document.getElementById("wins").textContent = wins
+    document.getElementById("losses").textContent = losses
+    document.getElementById("guessesRemaining").textContent = guessesRemaining
